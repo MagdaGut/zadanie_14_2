0c7c921948873846826e97aac0aac75947796f37
MagdaGut <36801717+MagdaGut@users.noreply.github.com>
2018-06-04 22:54:54 +0200

    Fixed script.js

--- script.js
+++ script.js
@@ -24,23 +24,42 @@ var movies = [
     title: 'Harry Potter',
     desc: 'Film o czarodzieju',
     genre: 'Fantasy, Przygodowy',
-    //posters.src: 'wizard.jpg';
+    posters: 'wizard.jpg'
   },
   {
     id: 2,
     title: 'Król Lew',
     desc: 'Film o królu sawanny',
     genre: 'Animacja',
-    //posters.src: 'lion.jpg';
+    posters: 'lion.jpg'
   }
-];  
+]; 
+
+/*
+React.createElement(
+  co tworzymy ?
+  jakie to ma właściwości ?
+  jakie są dzieci?
+)
+
+React.createElement(
+  'img',
+  {
+    src: 'lion.jpg',
+    width: 128,
+    height: 128,
+    title: "Lew",
+  },
+  null
+) 
+*/
 
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
       React.createElement('p', {}, movie.desc),
       React.createElement('p', {}, movie.genre),
-      React.createElement('img', {}, movie.posters),
+      React.createElement('img', {src: movie.posters, width: 128}),
     );
 });
 
@@ -49,3 +68,5 @@ var element =
     React.createElement('h1', {}, 'Lista filmów'),
     React.createElement('ul', {}, moviesElements)
   );
+
+ReactDOM.render(element, document.getElementById('app'));
